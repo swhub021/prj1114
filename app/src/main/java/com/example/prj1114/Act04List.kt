e96eda773abc78e3ebf280c77823d8ae2bc4f871
j <swhub021@gmail.com>
2022-12-01 10:53:11 +0900

    csw

--- app/src/main/java/com/example/prj1114/Act04List.kt
+++ app/src/main/java/com/example/prj1114/Act04List.kt
@@ -3,13 +3,18 @@ package com.example.prj1114
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.prj1114.databinding.Act4ListBinding
+import com.example.prj1114.databinding.Act5CreateBinding
 import kotlinx.android.synthetic.main.act4_list.*
 
 
 class Act04List : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act4_list)
+
 
         // Act05Create로 이동하는 플로팅 버튼.
         listToCreateFloatingActionButton.setOnClickListener {
--- app/src/main/java/com/example/prj1114/Act05Create.kt
+++ app/src/main/java/com/example/prj1114/Act05Create.kt
@@ -2,14 +2,21 @@ package com.example.prj1114
 
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.prj1114.databinding.Act5CreateBinding
+import com.example.prj1114.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.act5_create.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 
+const val TAG = "FIRESTORE"
 class Act05Create : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: Act5CreateBinding? = null
+
 
     private var cal = Calendar.getInstance()
 
@@ -19,11 +26,17 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act5_create)
 
+        binding = Act5CreateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+
 
         timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay: Int, minute: Int ->
             cal.set(Calendar.HOUR_OF_DAY, hourOfDay) // HOUR_OF_DAY과 MINUTE은 사용자가 선택한 시와 분
             cal.set(Calendar.MINUTE, minute)
             updateDateInView()
+            uploadData()
         }
         chosen_time.setOnClickListener(this)
     }
@@ -41,10 +54,31 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun uploadData() {
+        binding!!.btnSave.setOnClickListener {
+
+            // create a dummy data
+            val hashMap = hashMapOf<String, Any>(
+                "TIME" to chosen_time.text.toString()
+            )
+
+            // use the add() method to create a document inside users collection
+            FirebaseUtils().fireStoreDatabase.collection("users")
+                .add(hashMap)
+                .addOnSuccessListener {
+                    Log.d(TAG, "Added document with ID ${it.id}")
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error adding document $exception")
+                }
+        }
+    }
+
     // 사용자가 다이얼로그에서 선택한 시간이 텍스트 상자에 입력되는 되도록 함.
     private fun updateDateInView() {
         val myFormat = "h:mm a" // 입력되는 형식을 지정.
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault()) // A date format
         chosen_time.setText(sdf.format(cal.time).toString())
     }
+
 }
