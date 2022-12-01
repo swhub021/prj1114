3c3ceff39c422b3b59fde1209852c0e83519621c
j <swhub021@gmail.com>
2022-12-01 16:13:57 +0900

    csw

--- app/src/main/java/com/example/prj1114/Act04List.kt
+++ app/src/main/java/com/example/prj1114/Act04List.kt
@@ -2,9 +2,14 @@ package com.example.prj1114
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.prj1114.databinding.Act4ListBinding
 import com.example.prj1114.databinding.Act5CreateBinding
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import kotlinx.android.synthetic.main.act4_list.*
 
 
@@ -14,6 +19,9 @@ class Act04List : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act4_list)
+
+
+
 
 
         // Act05Create로 이동하는 플로팅 버튼.
--- app/src/main/java/com/example/prj1114/Act05Create.kt
+++ app/src/main/java/com/example/prj1114/Act05Create.kt
@@ -1,12 +1,22 @@
 package com.example.prj1114
 
+import android.app.Activity
 import android.app.TimePickerDialog
+import android.content.Intent
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.prj1114.databinding.Act5CreateBinding
 import com.example.prj1114.databinding.ActivityMainBinding
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import io.grpc.InternalChannelz.id
 import kotlinx.android.synthetic.main.act5_create.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -16,6 +26,9 @@ const val TAG = "FIRESTORE"
 class Act05Create : AppCompatActivity(), View.OnClickListener {
 
     private var binding: Act5CreateBinding? = null
+
+    private var mLatitude: Double = 0.0
+    private var mLongitude: Double = 0.0
 
 
     private var cal = Calendar.getInstance()
@@ -30,6 +43,14 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
 
+        if (!Places.isInitialized()) {
+            Places.initialize(
+                this@Act05Create,
+                resources.getString(R.string.google_maps_api_key), Locale.KOREA
+            )
+        }
+
+
 
 
         timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay: Int, minute: Int ->
@@ -39,6 +60,8 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
             uploadData()
         }
         chosen_time.setOnClickListener(this)
+        et_location.setOnClickListener(this)
+        btn_save.setOnClickListener(this)
     }
 
     // 전체 코드를 구현하는 곳.
@@ -51,15 +74,49 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.MINUTE), false
                 ).show()
             }
+
+            R.id.et_location -> {
+                try {
+                    // These are the list of fields which we required is passed
+                    val fields = listOf(
+                        Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG,
+                        Place.Field.ADDRESS
+                    )
+                    // Start the autocomplete intent with a unique request code.
+                    val intent =
+                        Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+                            .build(this@Act05Create)
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
+
+
     }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+
+                val place: Place = Autocomplete.getPlaceFromIntent(data!!)
+
+                et_location.setText(place.address)
+                mLatitude = place.latLng!!.latitude
+                mLongitude = place.latLng!!.longitude
+            }
+        }
+        }
 
     private fun uploadData() {
         binding!!.btnSave.setOnClickListener {
 
             // create a dummy data
             val hashMap = hashMapOf<String, Any>(
-                "TIME" to chosen_time.text.toString()
+                "TIME" to chosen_time.text.toString(),
+                "DEPARTURES" to et_location.text.toString()
             )
 
             // use the add() method to create a document inside users collection
@@ -74,6 +131,36 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            mLatitude = mLastLocation.latitude
+            Log.e("Current Latitude", "$mLatitude")
+            mLongitude = mLastLocation.longitude
+            Log.e("Current Longitude", "$mLongitude")
+
+            // TODO(Step 2: Call the AsyncTask class fot getting an address from the latitude and longitude.)
+            // START
+            val addressTask =
+                GetAddressFromLatLng(this@Act05Create, mLatitude, mLongitude)
+
+            addressTask.setAddressListener(object :
+                GetAddressFromLatLng.AddressListener {
+                override fun onAddressFound(address: String?) {
+                    Log.e("Address ::", "" + address)
+                    et_location.setText(address) // Address is set to the edittext
+                }
+
+                override fun onError() {
+                    Log.e("Get Address ::", "Something is wrong...")
+                }
+            })
+
+            addressTask.getAddress()
+            // END
+        }
+    }
+
     // 사용자가 다이얼로그에서 선택한 시간이 텍스트 상자에 입력되는 되도록 함.
     private fun updateDateInView() {
         val myFormat = "h:mm a" // 입력되는 형식을 지정.
@@ -81,4 +168,7 @@ class Act05Create : AppCompatActivity(), View.OnClickListener {
         chosen_time.setText(sdf.format(cal.time).toString())
     }
 
+    companion object {
+        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 3
+    }
 }
